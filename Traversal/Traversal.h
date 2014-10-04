54cb5e43a95f45645470f6727b7f7c7c75c03510
Rob Rix <rob.rix@github.com>
2014-10-04 16:35:21 -0400

    Project structure.

---
+++ Traversal/Traversal.h
@@ -0,0 +1,7 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Project version number for Traversal.
+extern double TraversalVersionNumber;
+
+/// Project version string for Traversal.
+extern const unsigned char TraversalVersionString[];
---
+++ TraversalTests/TraversalTests.swift
@@ -0,0 +1,6 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+import Traversal
+import XCTest
+
+class TraversalTests: XCTestCase {}
