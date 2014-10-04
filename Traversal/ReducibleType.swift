e50282fbdba852af148ef667f03983ea4c340f8f
Rob Rix <rob.rix@github.com>
2014-10-04 16:49:15 -0400

    reduce() over reducibles.

--- Traversal/ReducibleType.swift
+++ Traversal/ReducibleType.swift
@@ -14,3 +14,13 @@ public protocol ReducibleType {
 	/// The combine function returns Either in order to enable early termination; returning Either.Left(x) indicates that reduction should conclude immediately with x, whereas Either.Right(y) indicates that reduction should continue with y.
 	func reduceLeft<Result>(recur: (Self, Result, (Result, Element) -> Either<Result, Result>) -> Result) -> (Self, Result, (Result, Element) -> Either<Result, Result>) -> Result
 }
+
+
+/// Left-reduction of a reducible.
+func reduce<R : ReducibleType, Result>(collection: R, initial: Result, combine: (Result, R.Element) -> Either<Result, Result>) -> Result {
+	var recur: ((R, Result, (Result, R.Element) -> Either<Result, Result>) -> Result)!
+	recur = { collection, initial, combine in
+		collection.reduceLeft(recur)(collection, initial, combine)
+	}
+	return recur(collection, initial, combine)
+}
