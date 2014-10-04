d4b7d2973ee6af09175eb88717e68401be99d9f9
Rob Rix <rob.rix@github.com>
2014-10-04 16:37:55 -0400

    Define map over Either.

--- Traversal/Either.swift
+++ Traversal/Either.swift
@@ -4,3 +4,10 @@ public enum Either<T, U> {
 	case Left(Box<T>)
 	case Right(Box<U>)
 }
+
+func map<T>(either: Either<T, T>, f: T -> T) -> T {
+	switch either {
+	case let .Left(x): return x.value
+	case let .Right(x): return f(x.value)
+	}
+}
