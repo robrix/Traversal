a55cfe3ea8da61ce837a8e236b531ae5ec803f7b
Rob Rix <rob.rix@github.com>
2014-10-04 16:48:36 -0400

    The iteratees return Either.

--- Traversal/ReducibleType.swift
+++ Traversal/ReducibleType.swift
@@ -10,5 +10,7 @@ public protocol ReducibleType {
 	/// Instead of calling itself recursively as a typical left-reduce might do, the implementation calls out via the `recur` parameter instead. This allows the operator to be used for both enumeration (via a fixpoint) and iteration.
 	///
 	/// This is written in partially applied style to simplify the construction of enumerators.
-	func reduceLeft<Result>(recur: (Self, Result, (Result, Element) -> Result) -> Result) -> (Self, Result, (Result, Element) -> Result) -> Result
+	///
+	/// The combine function returns Either in order to enable early termination; returning Either.Left(x) indicates that reduction should conclude immediately with x, whereas Either.Right(y) indicates that reduction should continue with y.
+	func reduceLeft<Result>(recur: (Self, Result, (Result, Element) -> Either<Result, Result>) -> Result) -> (Self, Result, (Result, Element) -> Either<Result, Result>) -> Result
 }
