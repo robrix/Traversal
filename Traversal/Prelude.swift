da5f83fc7a9732140863acfaf56aea368d05edab
Rob Rix <rob.rix@github.com>
2014-10-05 10:52:36 -0400

    Add fix().
    h/t @CodaFi.

--- Traversal/Prelude.swift
+++ Traversal/Prelude.swift
@@ -4,3 +4,10 @@
 func id<T>(x: T) -> T {
 	return x
 }
+
+/// Returns a fixpoint of f(), i.e. a value x for which f(x) returns x.
+///
+/// This is used to make recursive reduce() from nonrecursive reduceLeft().
+public func fix<A, B>(f : (A -> B) -> A -> B) -> A -> B {
+	return { x in f(fix(f))(x) }
+}
