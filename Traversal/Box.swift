406ca7eba06167db68fc2dc112167a4b9e25ac25
Rob Rix <rob.rix@github.com>
2014-10-09 13:11:39 -0400

    Add a BoxType protocol.

--- Traversal/Box.swift
+++ Traversal/Box.swift
@@ -1,7 +1,7 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 /// A box to put a thing in.
-public final class Box<T> {
+public final class Box<T>: BoxType {
 	public let value: T
 	public init(_ v: T) {
 		value = v
