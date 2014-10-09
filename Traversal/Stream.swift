1b6fa078e1c06ea7755acfe87f034c22cde74ac2
Rob Rix <rob.rix@github.com>
2014-10-08 23:29:46 -0400

    Add first() and dropFirst() over Stream.

--- Traversal/Stream.swift
+++ Traversal/Stream.swift
@@ -6,3 +6,18 @@ public enum Stream<T> {
 	case Cons(Box<T>, @autoclosure () -> Stream<T>)
 }
 
+
+public func first<T>(stream: Stream<T>) -> T? {
+	switch stream {
+	case .Nil: return nil
+	case let .Cons(x, _): return x.value
+	}
+}
+
+public func dropFirst<T>(stream: Stream<T>) -> Stream<T> {
+	switch stream {
+	case .Nil: return .Nil
+	case let .Cons(_, rest): return rest()
+	}
+}
+
