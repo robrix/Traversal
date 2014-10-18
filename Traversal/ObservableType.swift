863268582eb8be1a521a66968a24d4dfe5040ac1
Rob Rix <rob.rix@github.com>
2014-10-18 00:59:31 -0400

    ObservableType connections can mutate.

--- Traversal/ObservableType.swift
+++ Traversal/ObservableType.swift
@@ -1,5 +1,5 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 public protocol ObservableType: ReducibleType {
-	func connect<S: SinkType where S.Element == Element>(sink: S)
+	mutating func connect<S: SinkType where S.Element == Element>(sink: S)
 }
