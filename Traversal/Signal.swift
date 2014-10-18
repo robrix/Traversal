b39f4ce59b6a1aef12285ba24511ddb8dab2f47d
Rob Rix <rob.rix@github.com>
2014-10-18 01:40:12 -0400

    Add a rough implementation of Signal.

---
+++ Traversal/Signal.swift
@@ -0,0 +1,27 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+public struct Signal<T>: ObservableType {
+	public init<R: ReducibleType>(_ reducible: R, _ map: R.Element -> T) {
+		var input = Stream<R.Element>(reducible)
+		source = SourceOf {
+			map(first(input)!)
+		}
+	}
+
+	// MARK: Reduction
+
+	public func reduceLeft<Result>(recur: Reducible<Signal, Result, T>.Enumerator) -> Reducible<Signal, Result, T>.Enumerator {
+		var output = Stream(source)
+		return { _, initial, combine in combine(initial, first(output)!).either(id, id) }
+	}
+
+	// MARK: Observation
+
+	public mutating func connect(observer: () -> ()) {
+		source.connect(observer)
+	}
+
+	// MARK: Implementation details
+
+	private var source: SourceOf<T>
+}
