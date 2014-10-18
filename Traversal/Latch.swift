b5ea6c8122737cf7892b23bb5d0be0295c150a8b
Rob Rix <rob.rix@github.com>
2014-10-17 22:46:27 -0400

    Latch implements ReducibleType.

--- Traversal/Latch.swift
+++ Traversal/Latch.swift
@@ -1,9 +1,13 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-struct Latch<T>: SinkType {
+struct Latch<T>: SinkType, ReducibleType {
 	var value: T
 
 	mutating func put(x: T) {
 		value = x
+	}
+
+	func reduceLeft<Result>(recur: Reducible<Latch, Result, T>.Enumerator) -> Reducible<Latch, Result, T>.Enumerator {
+		return { latch, initial, combine in combine(initial, latch.value).either(id, id) }
 	}
 }
