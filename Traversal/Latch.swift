db31b890f61ed38427e5f1cae07218d90755813e
Rob Rix <rob.rix@github.com>
2014-10-18 00:59:41 -0400

    Latch is Observable.

--- Traversal/Latch.swift
+++ Traversal/Latch.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-public struct Latch<T>: ReducibleType {
+public struct Latch<T>: ReducibleType, ObservableType {
 	public var value: T
 
 	public init(value: T) {
@@ -10,4 +10,14 @@ public struct Latch<T>: ReducibleType {
 	public func reduceLeft<Result>(recur: Reducible<Latch, Result, T>.Enumerator) -> Reducible<Latch, Result, T>.Enumerator {
 		return { latch, initial, combine in combine(initial, latch.value).either(id, id) }
 	}
+
+	// MARK: Observation
+
+	public mutating func connect<S : SinkType where S.Element == T>(var sink: S) {
+		observers.append {
+			sink.put($0)
+		}
+	}
+
+	private var observers: [T -> ()] = []
 }
