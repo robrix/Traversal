00c5f9012437a4c3fd485360aea5e2a317b30f9f
Rob Rix <rob.rix@github.com>
2014-10-18 00:59:05 -0400

    ObservableType doesn’t add its own Element type.

--- Traversal/ObservableType.swift
+++ Traversal/ObservableType.swift
@@ -1,6 +1,5 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 public protocol ObservableType: ReducibleType {
-	typealias Element
 	func connect<S: SinkType where S.Element == Element>(sink: S)
 }
