829d7861f585ef06b5cd0ec9983fc6a0cfc3606c
Rob Rix <rob.rix@github.com>
2014-10-18 00:18:11 -0400

    Construct Latches publicly.

--- Traversal/Latch.swift
+++ Traversal/Latch.swift
@@ -3,6 +3,10 @@
 public struct Latch<T>: ReducibleType {
 	public var value: T
 
+	public init(value: T) {
+		self.value = value
+	}
+
 	public func reduceLeft<Result>(recur: Reducible<Latch, Result, T>.Enumerator) -> Reducible<Latch, Result, T>.Enumerator {
 		return { latch, initial, combine in recur(latch, combine(initial, latch.value).either(id, id), combine) }
 	}
