9c5cb138965ae87c6a78811b48704153ccbc9399
Rob Rix <rob.rix@github.com>
2014-10-18 00:11:09 -0400

    Latch is public.

--- Traversal/Latch.swift
+++ Traversal/Latch.swift
@@ -1,9 +1,9 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-struct Latch<T>: ReducibleType {
-	var value: T
+public struct Latch<T>: ReducibleType {
+	public var value: T
 
-	func reduceLeft<Result>(recur: Reducible<Latch, Result, T>.Enumerator) -> Reducible<Latch, Result, T>.Enumerator {
-		return { latch, initial, combine in combine(initial, latch.value).either(id, id) }
+	public func reduceLeft<Result>(recur: Reducible<Latch, Result, T>.Enumerator) -> Reducible<Latch, Result, T>.Enumerator {
+		return { latch, initial, combine in recur(latch, combine(initial, latch.value).either(id, id), combine) }
 	}
 }
