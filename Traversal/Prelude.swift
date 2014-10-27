928d271e9bde6a147052a903ea909041e19a8320
Rob Rix <rob.rix@github.com>
2014-10-27 14:34:17 +0000

    Docs & style.

--- Traversal/Prelude.swift
+++ Traversal/Prelude.swift
@@ -5,9 +5,9 @@ func id<T>(x: T) -> T {
 	return x
 }
 
-/// Returns a fixpoint of f(), i.e. a value x for which f(x) returns x.
+/// Returns a fixpoint of a function f, i.e. a value x for which f(x) returns x.
 ///
 /// This is used to make recursive reduce() from nonrecursive reduceLeft().
-public func fix<A, B>(f : (A -> B) -> A -> B) -> A -> B {
+public func fix<A, B>(f: (A -> B) -> A -> B) -> A -> B {
 	return { x in f(fix(f))(x) }
 }
