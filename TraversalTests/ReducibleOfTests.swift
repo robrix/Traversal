eaf560120f8c45260b20227b7c3f821367db67b7
Rob Rix <rob.rix@github.com>
2014-10-29 21:45:27 +0000

    Rename a test.

--- TraversalTests/ReducibleOfTests.swift
+++ TraversalTests/ReducibleOfTests.swift
@@ -4,7 +4,7 @@ import Traversal
 import XCTest
 
 class ReducibleOfTests: XCTestCase {
-	func testReduction() {
+	func testConstructionFromSequenceType() {
 		let sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 		XCTAssertEqual(Traversal.reduce(ReducibleOf(sequence: sequence), 0, (+)), 45)
 	}
