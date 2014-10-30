e4ebbf83f77f6ce2b980d0976a9d3e7e029f90a1
Rob Rix <rob.rix@github.com>
2014-10-30 16:56:19 +0000

    Test ReducibleOf construction from ReducibleType.

--- TraversalTests/ReducibleOfTests.swift
+++ TraversalTests/ReducibleOfTests.swift
@@ -8,4 +8,11 @@ class ReducibleOfTests: XCTestCase {
 		let sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 		XCTAssertEqual(Traversal.reduce(ReducibleOf(sequence: sequence), 0, +), 45)
 	}
+
+	func testConstructionFromReducibleType() {
+		let sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+		let reducible = ReducibleOf(sequence: sequence)
+		let outer = ReducibleOf(reducible)
+		XCTAssertEqual(Traversal.reduce(outer, 0, +), 45)
+	}
 }
