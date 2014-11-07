d8b3cc208cc60742ad1ebc88884b29c428007812
Rob Rix <rob.rix@github.com>
2014-11-07 17:22:46 +0000

    Generalizes convolve to three reducibles.

--- Traversal/Convolve.swift
+++ Traversal/Convolve.swift
@@ -16,6 +16,22 @@ func convolve<R0: ReducibleType, R1: ReducibleType>(r0: R0, r1: R1) -> Stream<(R
 }
 
 
+/// Constructs the convolution of `r0`, `r1`, and `r2`.
+///
+/// This is effectiely a generalization of `map` to three reducibles.
+func convolve<R0: ReducibleType, R1: ReducibleType, R2: ReducibleType>(r0: R0, r1: R1, r2: R2) -> Stream<(R0.Element, R1.Element, R2.Element)> {
+	let streams = (Stream(r0), Stream(r1), Stream(r2))
+	return Stream {
+		switch (streams.0, streams.1, streams.2) {
+		case let (.Cons(x, _), .Cons(y, _), .Cons(z, _)):
+			return (x.value, y.value, z.value)
+		default:
+			return nil
+		}
+	}
+}
+
+
 // MARK: Imports
 
 import Box
