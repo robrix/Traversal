25492246685c47a5dd0823992799ef6ff8332a29
Rob Rix <rob.rix@github.com>
2014-11-07 18:31:49 +0000

    Avoid overflow errors when joining long reducibles.

--- Traversal/Join.swift
+++ Traversal/Join.swift
@@ -2,8 +2,10 @@
 
 /// Returns a reducer interleaving the elements of `reducible` with `separator`.
 public func join<R: ReducibleType>(separator: R.Element, reducible: R) -> ReducerOf<R, ReducibleOf<R.Element>> {
-	var iteration = 0
+	var firstIteration = true
 	return flattenMap(reducible) {
-		SequenceOf(iteration++ == 0 ? [$0] : [separator, $0])
+		let sequence = SequenceOf(firstIteration ? [$0] : [separator, $0])
+		firstIteration = false
+		return ReducibleOf(sequence: sequence)
 	}
 }
