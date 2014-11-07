b221efc567e9189699e14c8f91a62b86e5a3b3b9
Rob Rix <rob.rix@github.com>
2014-11-07 13:49:46 +0000

    Test all-inclusive filters.

---
+++ TraversalTests/FilterTests.swift
@@ -0,0 +1,13 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+import Traversal
+import XCTest
+
+class FilterTests: XCTestCase {
+	func testFilterAcceptingAll() {
+		let sequence = [1, 2, 3, 4]
+		let reducible = ReducibleOf(sequence: sequence)
+		let filtered = Traversal.filter(reducible, acceptAll)
+		XCTAssertEqual(reduce(filtered, 0, +), 10)
+	}
+}
