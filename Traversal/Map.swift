743b0f4a79069b89528ebc3c6772391c843cc513
Rob Rix <rob.rix@github.com>
2014-11-07 15:17:00 +0000

    Document map.

--- Traversal/Map.swift
+++ Traversal/Map.swift
@@ -1,5 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+/// Returns a reducer mapping the elements of `reducible` with `f`.
 public func map<Base: ReducibleType, T>(reducible: Base, f: Base.Element -> T) -> FlattenMapReducibleView<Base, ReducibleOf<T>> {
 	return flattenMap(reducible) {
 		GeneratorOfOne(f($0))
