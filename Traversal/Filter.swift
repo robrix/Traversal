a492c0088513ac1aa0f8d4b707ab91c7dfa41f79
Rob Rix <rob.rix@github.com>
2014-11-07 15:11:37 +0000

    Replace FilterReducibleView with flattenMap().

--- Traversal/Filter.swift
+++ Traversal/Filter.swift
@@ -1,47 +1,14 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-import Box
-
-public struct FilterReducibleView<Base: ReducibleType>: ReducibleType {
-	// MARK: ReducibleType
-
-	public func reducer<Result>() -> Reducible<Result, Base.Element>.Enumerator -> Reducible<Result, Base.Element>.Enumerator {
-		let reducer: Reducible<Result, Base.Element>.Enumerator -> Reducible<Result, Base.Element>.Enumerator = reducible.reducer()
-		return { recur in
-			var reduce: Reducible<Result, Base.Element>.Enumerator!
-			return { initial, combine in
-				if reduce == nil {
-					reduce = reducer { initial, _ in
-						recur(initial, combine)
-					}
-				}
-				return reduce(initial, self.transduce(combine))
-			}
-		}
-	}
-
-	// MARK: Private
-
-	/// Initializes with a reducible and a predicate function.
-	private init(reducible: Base, predicate: Base.Element -> Bool) {
-		self.reducible = reducible
-		self.predicate = predicate
-	}
-
-	private let reducible: Base
-	private let predicate: Base.Element -> Bool
-
-	private func transduce<Result>(combine: Reducible<Result, Base.Element>.Iteratee) -> Reducible<Result, Base.Element>.Iteratee {
-		let f = predicate
-		return { into, each in
-			f(each) ? combine(into, each) : .Right(Box(into))
-		}
-	}
-}
-
 /// Returns a reducer filtering out any elements of `reducible` which are not matched by `predicate`.
-public func filter<R: ReducibleType>(reducible: R, predicate: R.Element -> Bool) -> FilterReducibleView<R> {
-	return FilterReducibleView(reducible: reducible, predicate: predicate)
+public func filter<Base: ReducibleType>(reducible: Base, predicate: Base.Element -> Bool) -> FlattenMapReducibleView<Base, ReducibleOf<Base.Element>> {
+	let x = SequenceOf { GeneratorOfOne(1) }
+	let y = SequenceOf<Int> { GeneratorOf { nil } }
+	return flattenMap(reducible) { each in
+		predicate(each) ?
+			SequenceOf { GeneratorOfOne(each) }
+		:	SequenceOf { GeneratorOf { nil } }
+	}
 }
 
 
