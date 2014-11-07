326ee457342aafaf3f3796692d4370bc1db2f786
Rob Rix <rob.rix@github.com>
2014-11-07 15:19:44 +0000

    Document concat.

--- Traversal/FlattenMap.swift
+++ Traversal/FlattenMap.swift
@@ -43,6 +43,7 @@ public func flattenMap<Base: ReducibleType, T: SequenceType>(reducible: Base, ma
 	}
 }
 
+/// Returns a reducer concatenating the elements of `reducible`.
 public func concat<Base: ReducibleType where Base.Element: SequenceType>(reducible: Base) -> FlattenMapReducibleView<Base, ReducibleOf<Base.Element.Generator.Element>> {
 	return flattenMap(reducible, id)
 }
