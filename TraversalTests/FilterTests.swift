6ef61d819f35378d2ef61438bb2d8fbeb499e571
Rob Rix <rob.rix@github.com>
2014-11-07 13:50:15 +0000

    Test all-exclusive filters.

--- TraversalTests/FilterTests.swift
+++ TraversalTests/FilterTests.swift
@@ -10,4 +10,11 @@ class FilterTests: XCTestCase {
 		let filtered = Traversal.filter(reducible, acceptAll)
 		XCTAssertEqual(reduce(filtered, 0, +), 10)
 	}
+
+	func testFilterRejectingAll() {
+		let sequence = [1, 2, 3, 4]
+		let reducible = ReducibleOf(sequence: sequence)
+		let filtered = Traversal.filter(reducible, rejectAll)
+		XCTAssertEqual(reduce(filtered, 0, +), 0)
+	}
 }
