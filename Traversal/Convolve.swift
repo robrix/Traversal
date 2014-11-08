4024b1e8865d0ab93d0551c4ba5a2987c21fae0c
Rob Rix <rob.rix@github.com>
2014-11-08 06:37:44 -0500

    Typo.

--- Traversal/Convolve.swift
+++ Traversal/Convolve.swift
@@ -2,7 +2,7 @@
 
 /// Constructs the convolution of `r0` and `r1`.
 ///
-/// This is effectiely a generalization of `map` to two reducibles.
+/// This is effectively a generalization of `map` to two reducibles.
 public func convolve<R0: ReducibleType, R1: ReducibleType>(r0: R0, r1: R1) -> Stream<(R0.Element, R1.Element)> {
 	var streams = (Stream(r0), Stream(r1))
 	return Stream {
@@ -19,7 +19,7 @@ public func convolve<R0: ReducibleType, R1: ReducibleType>(r0: R0, r1: R1) -> St
 
 /// Constructs the convolution of `r0`, `r1`, and `r2`.
 ///
-/// This is effectiely a generalization of `map` to three reducibles.
+/// This is effectively a generalization of `map` to three reducibles.
 public func convolve<R0: ReducibleType, R1: ReducibleType, R2: ReducibleType>(r0: R0, r1: R1, r2: R2) -> Stream<(R0.Element, R1.Element, R2.Element)> {
 	var streams = (Stream(r0), Stream(r1), Stream(r2))
 	return Stream {
