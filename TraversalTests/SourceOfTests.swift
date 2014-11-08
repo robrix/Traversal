c8f7fb3def6b156dde621e3bbd40d7ac8ccaddda
Rob Rix <rob.rix@github.com>
2014-11-08 18:08:03 -0500

    Test that observers are notified of invalidation.

--- TraversalTests/SourceOfTests.swift
+++ TraversalTests/SourceOfTests.swift
@@ -4,13 +4,34 @@ import Traversal
 import XCTest
 
 class SourceOfTests: XCTestCase {
-	func testReducesOverItsCurrentSample() {
-		var effects = 0
-		let source = SourceOf {
-			effects
+	var effects: Int = 0 {
+		didSet {
+			source.invalidate()
 		}
+	}
+
+	private var source: SourceOf<Int>!
+
+	override func setUp() {
+		source = SourceOf { self.effects }
+		effects = 0
+	}
+
+	func testReducesOverItsCurrentSample() {
 		XCTAssertEqual(reduce(source, 0, +), 0)
 		effects = 1
 		XCTAssertEqual(reduce(source, 1, *), 1)
+	}
+
+	func testNotifiesObserversOnInvalidation() {
+		var observances = 0
+		source.connect {
+			observances += 1
+		}
+		XCTAssertEqual(observances, 0)
+		effects = 1
+		XCTAssertEqual(observances, 1)
+		effects = 12
+		XCTAssertEqual(observances, 2)
 	}
 }
