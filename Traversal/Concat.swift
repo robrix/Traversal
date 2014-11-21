c20a7b79b119b5656ef7add917b72b4286026135
Rob Rix <rob.rix@github.com>
2014-11-20 19:42:55 -0500

    Concat uses Stream instead of ReducibleOf.

--- Traversal/Concat.swift
+++ Traversal/Concat.swift
@@ -15,7 +15,7 @@ infix operator ++ {
 
 /// Returns a reducer concatenating the elements of `lhs` and `rhs`.
 public func ++ <R1: ReducibleType, R2: ReducibleType where R1.Element == R2.Element> (lhs: R1, rhs: R2) -> ReducibleOf<R1.Element> {
-	var generators = (ReducibleOf(lhs).generate(), ReducibleOf(rhs).generate())
+	var generators = (Stream(lhs).generate(), Stream(rhs).generate())
 	return ReducibleOf {
 		GeneratorOf {
 			generators.0.next() ?? generators.1.next()
