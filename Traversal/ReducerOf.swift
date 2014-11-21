daeea5605bf2504ef4b5ddc06c8357c8609df926
Rob Rix <rob.rix@github.com>
2014-11-20 22:56:07 -0500

    Memoize the transduction of flatten map blocks.

--- Traversal/ReducerOf.swift
+++ Traversal/ReducerOf.swift
@@ -17,16 +17,21 @@ public struct ReducerOf<Base: ReducibleType, T: ReducibleType>: ReducibleType {
 
 	public func reducer<Result>() -> Reducible<Result, T.Element>.Enumerator -> Reducible<Result, T.Element>.Enumerator {
 		let reducer: Reducible<Result, Base.Element>.Enumerator -> Reducible<Result, Base.Element>.Enumerator = reducible.reducer()
+		let transduce: Reducible<Result, T.Element>.Iteratee -> Reducible<Result, Base.Element>.Iteratee = flattenMapping(self.map)
 		return { recur in
 			// In order to reduce `reducible`, we have to pass it a `recur` function which calls the one which this function has been called with. We can’t just pass in `combine` because the element type of `reducible` does not match the element type of `self` (in the general case). However, we don’t want to generate a new function with every step, since that would be wasteful (and surprising). Therefore, we produce the function on the first step only. I’m discontent with this implementation, but it will have to do for now.
 			var reduce: Reducible<Result, Base.Element>.Enumerator!
+			var c: Reducible<Result, Base.Element>.Iteratee!
 			return { initial, combine in
+				if c == nil {
+					c = transduce(combine)
+				}
 				if reduce == nil {
 					reduce = reducer { initial, _ in
 						recur(initial, combine)
 					}
 				}
-				return reduce(initial, self.transduce(combine))
+				return reduce(initial, c)
 			}
 		}
 	}
@@ -36,12 +41,13 @@ public struct ReducerOf<Base: ReducibleType, T: ReducibleType>: ReducibleType {
 
 	private let reducible: Base
 	private let map: Base.Element -> T
+}
 
-	private func transduce<Result>(combine: Reducible<Result, T.Element>.Iteratee) -> Reducible<Result, Base.Element>.Iteratee {
-		let f = map
-		return { into, each in
-			.Right(Box(reduce(f(each), into, combine)))
-		}
+
+/// Transducer for flatten maps.
+private func flattenMapping<Result, U, T: ReducibleType>(map: U -> T)(combine: Reducible<Result, T.Element>.Iteratee) -> Reducible<Result, U>.Iteratee {
+	return { into, each in
+		return .Right(Box(reduce(map(each), into, combine)))
 	}
 }
 
