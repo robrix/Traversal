26230b9e6da909f519bccacb2a1f09ca01d0fe66
Rob Rix <rob.rix@github.com>
2014-11-23 14:14:41 -0500

    Test ReducerOf completeness.

---
+++ TraversalTests/ReducerOfTests.swift
@@ -0,0 +1,17 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+import Traversal
+import XCTest
+
+final class ReducerOfTests: XCTestCase {
+	let reducible = ReducibleOf(sequence: [[1, 2], [2, 3], [3, 4]])
+	lazy var reducer: ReducerOf<ReducibleOf<[Int]>, ReducibleOf<Int>> = {
+		return ReducerOf(self.reducible) {
+			ReducibleOf(sequence: $0)
+		}
+	}()
+
+	func testReducesEachElement() {
+		XCTAssertEqual(reduce(reducer, 0, +), 15)
+	}
+}
