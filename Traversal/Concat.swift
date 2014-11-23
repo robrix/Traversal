d316bb42c599f2f4c80a38fdaeb9125425c777f0
Rob Rix <rob.rix@github.com>
2014-11-23 18:15:43 -0500

    Concatenation over `ReducibleType`.

--- Traversal/Concat.swift
+++ Traversal/Concat.swift
@@ -1,6 +1,11 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-/// Returns a reducer concatenating the elements of `reducible`.
+/// Returns a reducer concatenating the `ReducibleType` elements of `reducible`.
+public func concat<R: ReducibleType where R.Element: ReducibleType>(reducible: R) -> ReducerOf<R, R.Element> {
+	return flattenMap(reducible, id)
+}
+
+/// Returns a reducer concatenating the `SequenceType` elements of `reducible`.
 public func concat<Base: ReducibleType where Base.Element: SequenceType>(reducible: Base) -> ReducerOf<Base, ReducibleOf<Base.Element.Generator.Element>> {
 	return flattenMap(reducible, id)
 }
