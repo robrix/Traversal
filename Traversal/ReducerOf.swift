8aab82f04cf736fe0023763869a7417b8beae6cd
Rob Rix <rob.rix@github.com>
2014-11-23 18:25:33 -0500

    ReducerOf transduces internally.

--- Traversal/ReducerOf.swift
+++ Traversal/ReducerOf.swift
@@ -17,21 +17,19 @@ public struct ReducerOf<Base: ReducibleType, T: ReducibleType>: ReducibleType, P
 
 	public func reducer<Result>() -> Reducible<Result, T.Element>.Enumerator -> Reducible<Result, T.Element>.Enumerator {
 		let reducer: Reducible<Result, Base.Element>.Enumerator -> Reducible<Result, Base.Element>.Enumerator = reducible.reducer()
-		let transduce: Reducible<Result, T.Element>.Iteratee -> Reducible<Result, Base.Element>.Iteratee = flattenMapping(self.map)
+		let map = self.map
+		var reduce: Reducible<Result, Base.Element>.Enumerator!
 		return { recur in
 			// In order to reduce `reducible`, we have to pass it a `recur` function which calls the one which this function has been called with. We can’t just pass in `combine` because the element type of `reducible` does not match the element type of `self` (in the general case). However, we don’t want to generate a new function with every step, since that would be wasteful (and surprising). Therefore, we produce the function on the first step only. I’m discontent with this implementation, but it will have to do for now.
-			var reduce: Reducible<Result, Base.Element>.Enumerator!
-			var c: Reducible<Result, Base.Element>.Iteratee!
 			return { initial, combine in
-				if c == nil {
-					c = transduce(combine)
-				}
 				if reduce == nil {
 					reduce = reducer { initial, _ in
 						recur(initial, combine)
 					}
 				}
-				return reduce(initial, c)
+				return reduce(initial) {
+					.right(Traversal.reduce(map($1), $0, combine))
+				}
 			}
 		}
 	}
@@ -48,10 +46,4 @@ public struct ReducerOf<Base: ReducibleType, T: ReducibleType>: ReducibleType, P
 
 	private let reducible: Base
 	private let map: Base.Element -> T
-}
-
-
-/// Transducer for flatten maps.
-private func flattenMapping<Result, U, T: ReducibleType>(map: U -> T)(combine: Reducible<Result, T.Element>.Iteratee) -> Reducible<Result, U>.Iteratee {
-	return { into, each in .right(reduce(map(each), into, combine)) }
 }
