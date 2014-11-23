19e69199eb8960d5143863442d1de2631cc561e7
Rob Rix <rob.rix@github.com>
2014-11-23 18:29:34 -0500

    Lots more tests of concatenation.

--- TraversalTests/ConcatTests.swift
+++ TraversalTests/ConcatTests.swift
@@ -4,16 +4,61 @@ import Traversal
 import XCTest
 
 class ConcatTests: XCTestCase {
-	func testConcat() {
+	func test0aryConcat() {
+		let reducible = ReducibleOf(sequence: [ ReducibleOf(sequence: []) ])
+		XCTAssertEqual(reduce(map(concat(reducible), toString), "", +), "")
+	}
+
+	func test1aryConcat() {
+		let reducible = ReducibleOf(sequence: [ ReducibleOf(sequence: [1]) ])
+		XCTAssertEqual(reduce(map(concat(reducible), toString), "", +), "1")
+	}
+
+	func test2aryConcat() {
+		let reducible = concat(ReducibleOf(sequence: [ ReducibleOf(sequence: [1, 2]) ]))
+		XCTAssertEqual(reduce(reducible, "0") { $0 + toString($1) }, "012")
+	}
+
+	func test2arySplitConcat() {
+		let reducible = ReducibleOf(sequence: [ ReducibleOf(sequence: [1]), ReducibleOf(sequence: [2]) ])
+		XCTAssertEqual(reduce(map(concat(reducible), toString), "0", +), "012")
+	}
+
+	func test3aryConcat() {
+		let reducible = ReducibleOf(sequence: [ ReducibleOf(sequence: [1, 2, 3]) ])
+		XCTAssertEqual(reduce(map(concat(reducible), toString), "0", +), "0123")
+	}
+
+	func test3arySplitConcat() {
+		let reducible = ReducibleOf(sequence: [ ReducibleOf(sequence: [1]), ReducibleOf(sequence: [2]), ReducibleOf(sequence: [3]) ])
+		XCTAssertEqual(reduce(map(concat(reducible), toString), "0", +), "0123")
+	}
+	
+	func testConcatOverSequences() {
 		let sequence = [[1, 2], [3], [], [4]]
 		let reducible = ReducibleOf(sequence: sequence)
 		let concatenated = concat(reducible)
 		XCTAssertEqual(reduce(concatenated, 0, +), 10)
+		let mapped = map(concatenated, toString)
+		let joined = join(", ", mapped)
+		XCTAssertEqual(reduce(joined, "0", +), "01, 2, 3, 4")
+	}
+
+	func testConcatOverReducibles() {
+		let sequence = [[1, 2], [3], [], [4]]
+		let reducible = ReducibleOf(sequence: map(sequence) {
+			ReducibleOf(sequence: $0)
+		})
+		let concatenated = concat(reducible)
+		XCTAssertEqual(reduce(concatenated, 0, +), 10)
+		let mapped = map(concatenated, toString)
+		let joined = join(", ", mapped)
+		XCTAssertEqual(reduce(joined, "0", +), "01, 2, 3, 4")
 	}
 
 	func testInfixConcatenation() {
 		let concatenated = ReducibleOf(sequence: [10, 5, 3]) ++ ReducibleOf(sequence: [2, 20, 20])
-		let mapped = Traversal.map(concatenated, toString)
-		XCTAssertEqual(Traversal.reduce(mapped, "", +), "105322020")
+		let mapped = join(", ", map(concatenated, toString))
+		XCTAssertEqual(reduce(mapped, "0", +), "010, 5, 3, 2, 20, 20")
 	}
 }
