e9f1dcdf07d417cf28751ae624e604da8754a339
Rob Rix <rob.rix@github.com>
2014-11-30 19:01:16 -0500

    Remove a no-longer-applicable comment.

--- Traversal/ReducerOf.swift
+++ Traversal/ReducerOf.swift
@@ -17,7 +17,6 @@ public struct ReducerOf<Base: ReducibleType, T: ReducibleType>: ReducibleType, P
 
 	public func reducer<Result>() -> Reducible<ReducerOf, Result, T.Element>.Enumerator -> Reducible<ReducerOf, Result, T.Element>.Enumerator {
 		return { recur in
-			// In order to reduce `reducible`, we have to pass it a `recur` function which calls the one which this function has been called with. We can’t just pass in `combine` because the element type of `reducible` does not match the element type of `self` (in the general case). However, we don’t want to generate a new function with every step, since that would be wasteful (and surprising). Therefore, we produce the function on the first step only. I’m discontent with this implementation, but it will have to do for now.
 			{ collection, initial, combine in
 				(collection.reducible.reducer()) { reducible, initial, _ in
 					recur(ReducerOf(reducible, collection.map), initial, combine)
