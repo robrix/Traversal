9b145f9f92b8cccb511c97e59bcc5c3a52f4eb04
Rob Rix <rob.rix@github.com>
2014-12-01 05:34:00 -0500

    Remove all the uncurried composition forms.

--- Traversal/Prelude.swift
+++ Traversal/Prelude.swift
@@ -28,40 +28,15 @@ public func .. <T, U, V, W>(f: V -> W, g: T -> U -> V) -> (T, U) -> W {
 	return uncurry { f .. g($0) }
 }
 
-/// Unary/binary function composition (uncurried).
-public func .. <T, U, V, W>(f: V -> W, g: (T, U) -> V) -> (T, U) -> W {
-	return f .. curry(g)
-}
-
 /// Binary/unary function composition (curried).
 public func .. <T, U, V, W>(f: U -> V -> W, g: T -> U) -> (T, V) -> W {
 	return uncurry(f .. g)
-}
-
-/// Binary/unary function composition (uncurried).
-public func .. <T, U, V, W>(f: (U, V) -> W, g: T -> U) -> (T, V) -> W {
-	return curry(f) .. g
 }
 
 
 /// Binary/binary function composition (curried, curried).
 public func .. <T, U, V, W, X>(f: V -> W -> X, g: T -> U -> V) -> (T, U, W) -> X {
 	return uncurry { f .. g($0) }
-}
-
-/// Binary/binary function composition (curried, uncurried).
-public func .. <T, U, V, W, X>(f: V -> W -> X, g: (T, U) -> V) -> (T, U, W) -> X {
-	return f .. curry(g)
-}
-
-/// Binary/binary function composition (uncurried, curried).
-public func .. <T, U, V, W, X>(f: (V, W) -> X, g: T -> U -> V) -> (T, U, W) -> X {
-	return curry(f) .. g
-}
-
-/// Binary/binary function composition (uncurried, uncurried).
-public func .. <T, U, V, W, X>(f: (V, W) -> X, g: (T, U) -> V) -> (T, U, W) -> X {
-	return curry(f) .. curry(g)
 }
 
 
