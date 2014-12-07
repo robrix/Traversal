9a947385c6d4b853e2c40487430b7408672a6caa
Rob Rix <rob.rix@github.com>
2014-12-07 18:28:02 -0500

    Correct the documentation of +=.

--- Traversal/IntegratorType.swift
+++ Traversal/IntegratorType.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-/// Appends `collection` with the elements of a `reducible`.
+/// Extends `collection` with the elements of a `reducible`.
 public func += <E: ExtensibleCollectionType, R: ReducibleType where R.Element == E.Generator.Element> (inout left: E, right: R) {
 	reduce(right, ()) {
 		left.append($1)
