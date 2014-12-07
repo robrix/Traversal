a759c0ae2436cc721d7329fcbad203277446639a
Rob Rix <rob.rix@github.com>
2014-12-07 13:04:17 -0500

    Test ternary zip.

--- TraversalTests/ZipTests.swift
+++ TraversalTests/ZipTests.swift
@@ -1,5 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+import Prelude
 import Traversal
 import XCTest
 
@@ -9,5 +10,11 @@ class ZipTests: XCTestCase {
 		let zipped = zip(r, r)
 		let mapped = Traversal.map(zipped, +)
 		XCTAssertEqual(reduce(mapped, 0, +), 12)
+	}
+
+	func testZipOfThreeParameters() {
+		let s: Stream = [1, 2, 3]
+		let zipped = zip(s, map(s, 2 |> curry(*)), map(s, 3 |> curry(*)))
+		XCTAssertEqual(reduce(Traversal.map(zipped, toString), "", (+)), "(1, 2, 3)(2, 4, 6)(3, 6, 9)")
 	}
 }
